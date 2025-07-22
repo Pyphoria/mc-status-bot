f4e71c78e0317cd50dd0e8829f1893fcfd7bfb49
Knockout341 <Z.ocken@gmx.de>
2025-07-23 00:12:19 +0200

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Ich bin wach! 🟢"
+    return "I'm awake!"
 
 def keep_alive():
     t = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
